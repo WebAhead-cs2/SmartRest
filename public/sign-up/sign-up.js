6679d48de4b0cbf27930706eb58d253b956ef750
saherhanif <saher.ha97@gmail.com>
2022-12-05 21:11:12 +0200

    updated error handling

--- public/sign-up/sign-up.js
+++ public/sign-up/sign-up.js
@@ -1,9 +1,9 @@
 function validation() {
   let message = document.getElementById("errors");
-  let name = document.getElementById("name");
-  let email = document.getElementById("email");
-  let password = document.getElementById("password");
-  let password2 = document.getElementById("password2");
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  let password2 = document.getElementById("password2").value;
 
   message.innerHTML = "";
   console.log(name, email, password, password2);
@@ -11,10 +11,9 @@ function validation() {
     if (!name || !email || !password || !password2)
       throw "please enter all fields";
 
-    if (password.length < 6)
-      throw "password should be at least 6 charcters";
+    else if  (password.length < 6) throw "password should be at least 6 charcters";
 
-    if (password !== password2) throw "passwords doesnt match";
+    else if (password !== password2) throw "passwords doesnt match";
   } catch (err) {
     message.innerHTML = err;
   }
--- server.js
+++ server.js
@@ -1,74 +1,61 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const {Pool, pool} = require('./dbConfig');
+const { Pool, pool } = require("./dbConfig");
 const PORT = 3000;
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
+app.use("/public", express.static(__dirname + "/public"));
+app.use("/menu", express.static(__dirname + "/public/menu"));
+app.use("/sign-up", express.static(__dirname + "/public/sign-up"));
+app.use(express.static(__dirname + "/public/sign-in"));
 
-app.use('/public',express.static(__dirname + '/public'));
-app.use('/menu',express.static(__dirname + '/public/menu'));
-app.use('/sign-up',express.static(__dirname + '/public/sign-up'));
-app.use(express.static(__dirname + '/public/sign-in'));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/sign-in/sign-in.html");
+});
 
+app.get("/menu", (req, res) => {
+  res.sendFile(__dirname + "/public/menu/menu.html");
+});
 
-app.get('/', (req, res) => {
+app.get("/sign-up", (req, res) => {
+  res.sendFile(__dirname + "/public/sign-up/sign-up.html");
+});
 
-  res.sendFile(__dirname + '/public/sign-in/sign-in.html');
+app.post("/sign-up", (req, res) => {
+  let { name, email, password } = req.body;
+  console.log(req.body);
+  console.log({ name, email, password });
   
 });
+// //  let errors =[];
+// try{
 
-app.get('/menu', (req, res) => {
+//   // validators
+//  if(!name || !email || !password || !password2) throw new Error("please enter all fields");
+// //  {
+// //   errors.push({messege: "please enter all fields"})
+// //  }
+//  if(password.length <6) throw new Error("password should be at least 6 charcters");
+// //  {
+// //   errors.push({messege: "password should be at least 6 charcters"})
+// //  }
+//  if(password !== password2) throw new Error("passwords doesnt match");
+// //  {
 
-    res.sendFile(__dirname + '/public/menu/menu.html');
-  
-});
+//  db connect and check if user exist
 
-app.get('/sign-up', (req, res) => {
+// db insert ...
+// res.redirect()
 
-  res.sendFile(__dirname + '/public/sign-up/sign-up.html');
-
-});
-
- app.post('/sign-up', (req,res)=>{
-   let { name,email,password} = req.body;
-   console.log(req.body);
-   console.log({name,email,password});
-  //  if (pool.)
-   
-  });
-   // //  let errors =[];
-  // try{
-
-  //   // validators
-  //  if(!name || !email || !password || !password2) throw new Error("please enter all fields");
-  // //  {
-  // //   errors.push({messege: "please enter all fields"})
-  // //  } 
-  //  if(password.length <6) throw new Error("password should be at least 6 charcters");
-  // //  {
-  // //   errors.push({messege: "password should be at least 6 charcters"})
-  // //  }
-  //  if(password !== password2) throw new Error("passwords doesnt match");
-  // //  {
-
-
-  //  db connect and check if user exist
-
-
-  // db insert ... 
-  // res.redirect()
-
-
-  //   errors.push({messege: "passwords doesnt match"})
-  //  }
-  //  if(errors.length>0)
-  //  {
-  //  } catch(err) {
-  //   res.json(err.message)
-  // }
-  //   res.send({errors},"/public/sign-up/sign-up.html")
-  //  }
-  //  console.log(errors)
-
+//   errors.push({messege: "passwords doesnt match"})
+//  }
+//  if(errors.length>0)
+//  {
+//  } catch(err) {
+//   res.json(err.message)
+// }
+//   res.send({errors},"/public/sign-up/sign-up.html")
+//  }
+//  console.log(errors)
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
