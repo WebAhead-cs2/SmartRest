4efb19c2e1c0f451783273f382f559e441c25c7f
MuhammadAw90 <Muhammad.Awawdy90@gmail.com>
2022-12-05 20:15:44 +0200

    uploading new menu

--- dbConfig.js
+++ dbConfig.js
@@ -1,10 +1,11 @@
-require("dotenv").config();
-const {Pool} = require("pg");
-const isProduction = process.env.NODE_ENV ==="production";
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const isProduction = process.env.NODE_ENV === 'production';
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.BD_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL: connectionString
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
 });
 
-module.exports = {pool};
+module.exports = { pool };
