349e2fcdcd03d717663dc117141ff3361ceaabf7
MuhammadAw90 <Muhammad.Awawdy90@gmail.com>
2022-12-06 14:37:41 +0200

    add the handlers and router

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+const http = require("http");
+const router = require("./router");
 const express = require('express');
+const PORT = 4000;
+
+const server = http.createServer(router);
 
 const app = express();
-const { Pool } = require('./database/dbConfig');
+const { Pool, pool } = require('./database/dbConfig');
 
-const PORT = 4000;
+
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${__dirname}/public`));
@@ -22,6 +27,43 @@ app.get('/menu', (req, res) => {
 app.get('/sign-up', (req, res) => {
   res.sendFile(`${__dirname}/public/sign-up/sign-up.html`);
 });
+
+app.post("/sign-up", (req, res) => {
+  let { name, email, password } = req.body;
+  console.log(req.body);
+  console.log({ name, email, password });
+
+});
+
+// //  let errors =[];
+// try{
+//   // validators
+//  if(!name || !email || !password || !password2) throw new Error("please enter all fields");
+// //  {
+// //   errors.push({messege: "please enter all fields"})
+// //  }
+//  if(password.length <6) throw new Error("password should be at least 6 charcters");
+// //  {
+// //   errors.push({messege: "password should be at least 6 charcters"})
+// //  }
+//  if(password !== password2) throw new Error("passwords doesnt match");
+// //  {
+
+//  db connect and check if user exist
+
+// db insert ...
+// res.redirect()
+
+//   errors.push({messege: "passwords doesnt match"})
+//  }
+//  if(errors.length>0)
+//  {
+//  } catch(err) {
+//   res.json(err.message)
+// }
+//   res.send({errors},"/public/sign-up/sign-up.html")
+//  }
+//  console.log(errors)
 
 app.post('/public/sign-up/sign-up.html', (req, res) => {
   const { name, email, password } = req.body;
