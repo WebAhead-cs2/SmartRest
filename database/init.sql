8ab6a4379e57d2241be06a09f2c24467fe95682b
saherhanif <saher.ha97@gmail.com>
2022-12-07 23:03:56 +0200

    fixed sign up page/log in page

--- database/init.sql
+++ database/init.sql
@@ -5,21 +5,21 @@ DROP TABLE IF EXISTS users, blog_posts CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   username VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE reviews (
   id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES FOREIGN KEY users(id),
-  Review TEXT,
-  PostDate dateTime.now
+  user_id INTEGER REFERENCES users(id),
+  Review TEXT
+  -- PostDate dateTime.now
 );
 
-INSERT INTO users (username, email, password) VALUES
-;
+--  INSERT INTO users (username, email, password) VALUES ('saher', 'heythere@gmail.com', '123456');
 
-INSERT INTO reviews (text_content, user_id,Review,PostDate) VALUES
-;
+-- INSERT INTO reviews (text_content, user_id,Review,PostDate) VALUES
+-- ;
 
 COMMIT;
+
---
+++ dbConfig.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/newline-after-import */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable comma-dangle */
+/* eslint-disable space-infix-ops */
+/* eslint-disable quotes */
+require("dotenv").config();
+const {Pool} = require("pg");
+const isProduction = process.env.NODE_ENV ==="production";
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.BD_PORT}/${process.env.DB_DATABASE}`;
+
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL: connectionString
+});
+
+module.exports = {pool};
